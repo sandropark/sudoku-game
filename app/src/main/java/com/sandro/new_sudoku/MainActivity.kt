1b54c9bdcde04acd7ea8b907d36318499deb7b49
sandropark <ticoba@naver.com>
2025-07-26 17:48:37 +0900

    프리뷰 추가

--- app/src/main/java/com/sandro/new_sudoku/MainActivity.kt
+++ app/src/main/java/com/sandro/new_sudoku/MainActivity.kt
@@ -7,19 +7,31 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.sandro.new_sudoku.ui.theme.New_sudokuTheme
+import androidx.compose.ui.tooling.preview.Preview
+import com.sandro.new_sudoku.ui.theme.SudokuTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            New_sudokuTheme {
+            SudokuTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     SudokuScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SudokuPreview() {
+    SudokuTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            SudokuScreen(modifier = Modifier.padding(innerPadding))
         }
     }
 }
